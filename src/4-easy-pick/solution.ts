dfd861372d6b3647e4789cee14c614fd4eed486b
luigi2077 <chenyuan_hu@outlook.com>
2022-02-17 17:41:53 +0800

    pick & readonly

---
+++ src/4-easy-pick/solution.ts
@@ -0,0 +1,27 @@
+import { Expect, Equal} from "@type-challenges/utils"
+type MyPick<T, K extends keyof T> = {[P in K ]: T[P]}
+
+
+
+
+type cases = [
+    Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+    Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+    // build error example
+    // MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+  ]
+  
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+  
+  interface Expected1 {
+    title: string
+  }
+  
+  interface Expected2 {
+    title: string
+    completed: boolean
+  }
